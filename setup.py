c20769994830db42a645996b391f8ce57fb11541
PabloNA97 <p.navarro.acero@gmail.com>
2024-06-11 16:55:47 +0200

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,17 @@ from setuptools import setup, find_packages
 
 setup(name='mlcolvar_utils', 
     version='0.1', 
-    packages=find_packages(include=['mlcolvar_utils']))
+    packages=find_packages(include=['mlcolvar_utils']),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'pytorch',
+        'pydantic<2',
+        'lightning',
+        'mdanalysis',
+        'scikit-learn>=1.3',
+        'seaborn',
+        'scipy',
+        'matplotlib',
+        'mlcolvar'
+    ])
